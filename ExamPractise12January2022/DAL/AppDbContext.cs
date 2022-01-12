d67777e1e4a7dacd3e7b86425db8ee7a4ac84f43
Cavid Haciyev <cavid.haciyev.2002@mail.ru>
2022-01-12 19:44:41 +0400

    reformat

--- ExamPractise12January2022/DAL/AppDbContext.cs
+++ ExamPractise12January2022/DAL/AppDbContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ExamPractise12January2022.Models;
+﻿using ExamPractise12January2022.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExamPractise12January2022.DAL
--- ExamPractise12January2022/Models/WhyChoose.cs
+++ ExamPractise12January2022/Models/WhyChoose.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
 namespace ExamPractise12January2022.Models
 {
     public class WhyChoose
